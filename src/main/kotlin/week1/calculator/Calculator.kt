208b6c8bd3bade8662afebb3506bafc1bdc3a21e
Sara Han <sarahan774@gmail.com>
2024-11-22 07:20:34 +0900

    [step2] 문자열 계산기 코드리뷰 반영 - Operands 클래스 제거 - BinaryOperator 인터페이스 사용 - Operator 파일 분리

--- src/main/kotlin/week1/calculator/Calculator.kt
+++ src/main/kotlin/week1/calculator/Calculator.kt
@@ -3,15 +3,13 @@ package week1.calculator
 import java.util.LinkedList
 
 class Calculator {
-    private val minInputTokens = 3
-
     fun calculate(input: String?): String {
         val queue = parseInput(input)
         while (queue.size > 1) {
             val x = queue.poll()
             val op = queue.poll()
             val y = queue.poll()
-            val result = Operator.from(op).applyTo(Operands(x, y)).toString()
+            val result = Operator.from(op).apply(x.toInt(), y.toInt()).toString()
             queue.addFirst(result)
         }
         return queue.poll()
@@ -23,36 +21,12 @@ class Calculator {
         }
 
         return input!!.split(" ")
-            .takeIf { it.size >= minInputTokens }
+            .takeIf { it.size >= MIN_INPUT_TOKENS }
             ?.toCollection(LinkedList())
             ?: throw IllegalStateException("At least 2 operands and 1 operator is required.")
     }
 
-    enum class Operator(private val symbol: String, val applyTo: (Operands) -> Int) {
-        Plus("+", { it.x + it.y }),
-        Minus("-", { it.x - it.y }),
-        Divide("/", { it.x / it.y }),
-        Multiply("*", { it.x * it.y }),;
-
-        companion object {
-            fun from(input: String): Operator {
-                return entries.find { it.symbol == input } ?: throw IllegalArgumentException("Invalid Operator $input")
-            }
-        }
-    }
-
-    data class Operands(
-        private val _x: String,
-        private val _y: String,
-    ) {
-        val x: Int = getIntOrThrow(_x)
-        val y: Int = getIntOrThrow(_y)
-
-        private fun getIntOrThrow(n: String): Int {
-            require(n.isNotEmpty() || n.isNotBlank()) {
-                "Invalid operand format $n"
-            }
-            return n.toInt()
-        }
+    companion object {
+        private const val MIN_INPUT_TOKENS = 3
     }
 }
