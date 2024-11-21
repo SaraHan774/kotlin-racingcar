37ff84004c3380f358ebdb9e0b9dfeea395f0ad8
Sara Han <sarahan774@gmail.com>
2024-11-22 07:56:26 +0900

    [step3] RacingGame 테스트 보강 - 엣지케이스 처리 (입력값 0일 경우) - 게임 레코드 검증 추가

--- src/main/kotlin/week1/racing/RacingGame.kt
+++ src/main/kotlin/week1/racing/RacingGame.kt
@@ -2,20 +2,24 @@ package week1.racing
 
 class RacingGame(
     numCars: Int,
-    private val numTries: Int,
+    private val numRounds: Int,
 ) {
     private val cars = List(numCars) { RacingCar() }
     private val _gameRounds = mutableListOf<GameRound>()
     val gameRounds: List<GameRound> = _gameRounds
 
     fun start() {
-        repeat(numTries) { round ->
+        require(cars.isNotEmpty() && numRounds > 0) {
+            "Game cannot be started! (cars.size = ${cars.size}, numRounds = $numRounds)"
+        }
+
+        repeat(numRounds) { round ->
             val positions = mutableListOf<Int>()
             cars.forEach { car ->
                 car.moveForward()
                 positions.add(car.currentPosition)
             }
-            _gameRounds.add(GameRound(id = round, records = positions))
+            _gameRounds.add(GameRound(id = round + 1, records = positions))
         }
     }
 }
--- src/test/kotlin/week1/racing/RacingGameTest.kt
+++ src/test/kotlin/week1/racing/RacingGameTest.kt
@@ -1,25 +1,52 @@
 package week1.racing
 
-import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import kotlin.random.Random
-import kotlin.random.nextInt
+import org.junit.jupiter.api.assertThrows
 
 class RacingGameTest {
+    // 초기화 검증 - 자동차 or 시도 횟수 0 인 경우
     @Test
-    fun `{given} 자동차 대수 0대 시도 횟수 0번 {when} start() {then} gameRounds 는 Empty 상태`() {
-        val game = RacingGame(0, 0)
-        game.start()
-        assertThat(game.gameRounds.isEmpty()).isTrue()
+    fun `{given} numCar = 0, numRounds = 0 {when} start() {then} gameRounds isEmpty`() {
+        val game = RacingGame(numCars = 0, numRounds = 0)
+        assertThrows<IllegalArgumentException> { game.start() }
     }
 
     @Test
-    fun `{given} 자동차 1대 이상 시도 횟수 1회 이상 {when} start() {then} records size == 자동차 대수, gameRounds size == 시도횟수`() {
-        val numCar = Random.nextInt(1..10)
-        val numTries = Random.nextInt(1..10)
-        val game = RacingGame(numCar, numTries)
+    fun `{given} numCar = 0, numRounds = 5 {when} start() {then} gameRounds isEmpty`() {
+        val game = RacingGame(numCars = 0, numRounds = 5)
+        assertThrows<IllegalArgumentException> { game.start() }
+    }
+
+    @Test
+    fun `{given} numCars = 5, numRounds = 0 {when} start() {then} gameRounds isEmpty`() {
+        val game = RacingGame(numCars = 5, numRounds = 0)
+        assertThrows<IllegalArgumentException> { game.start() }
+    }
+
+    @Test
+    fun `{given} numCars = 3, numRounds = 5 {when} start() {then} records size == 3, gameRounds size == 5`() {
+        val numCar = 3
+        val numRounds = 5
+        val game = RacingGame(numCar, numRounds)
+
         game.start()
-        assertThat(game.gameRounds.size).isEqualTo(numTries)
-        assertThat(game.gameRounds[0].records.size).isEqualTo(numCar)
+
+        assertEquals(numCar, game.gameRounds[0].records.size)
+        assertEquals(numRounds, game.gameRounds.size)
+    }
+
+    // record 검증
+    @Test
+    fun `{given} numCars = 2, numRounds = 1 {when} start() {then} gameRound id = 1, records size == 2`() {
+        val numCars = 2
+        val numRounds = 1
+        val racingGame = RacingGame(numCars, numRounds)
+
+        racingGame.start()
+
+        val gameRound = racingGame.gameRounds.first()
+        assertEquals(1, gameRound.id)
+        assertEquals(numCars, gameRound.records.size)
     }
 }
