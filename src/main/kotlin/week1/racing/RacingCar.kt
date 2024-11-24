3bc5c375b2d875ba56efcefaff1fb0fc2cbd5a9b
Sara Han <sarahan774@gmail.com>
2024-11-24 20:41:22 +0900

    [step4]  각 자동차 이름은 최대 5자까지 허용됨. 관련 테스트 추가.

--- src/main/kotlin/week1/racing/RacingCar.kt
+++ src/main/kotlin/week1/racing/RacingCar.kt
@@ -3,6 +3,12 @@ package week1.racing
 import kotlin.random.Random
 
 class RacingCar(val name: String) {
+    init {
+        require(name.length <= MAX_CAR_NAME_LENGTH) {
+            "Car name is too long ! (max length = $MAX_CAR_NAME_LENGTH, current = ${name.length})"
+        }
+    }
+
     var currentPosition = 1
         private set
 
@@ -16,5 +22,9 @@ class RacingCar(val name: String) {
         val randomUntil = 9
         val minForwardThreshold = 4
         return Random.nextInt(until = randomUntil) >= minForwardThreshold
+    }
+
+    companion object {
+        private const val MAX_CAR_NAME_LENGTH = 5
     }
 }
